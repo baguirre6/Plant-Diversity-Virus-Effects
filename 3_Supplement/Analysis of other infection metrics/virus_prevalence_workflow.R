883828102b947a6b04b547412abef288c066e360
baguirre6 <baguirre9393@gmail.com>
2025-07-10 10:10:44 -0400

    adjusted weights and added annotations to results that match manuscript supplement table numbers

--- 3_Supplement/Analysis of other infection metrics/virus_prevalence_workflow.R
+++ 3_Supplement/Analysis of other infection metrics/virus_prevalence_workflow.R
@@ -45,6 +45,16 @@ virus.data %>%
   group_by(Year, Block, Plot, FunctionalDiversity, GrassType, fg_richness, Species)%>% 
   summarise(mean.plot.infection = mean(PAV_Infection, na.rm=TRUE)) -> mean.spp.BYDV
 
+#Add weights column
+mean.spp.BYDV$weights <- 10
+
+#Adjust weights for species with less than 10 samples :
+mean.spp.BYDV$weights[90] <- 7
+mean.spp.BYDV$weights[127] <- 7
+mean.spp.BYDV$weights[133] <- 3
+mean.spp.BYDV$weights[141] <- 8
+mean.spp.BYDV$weights[160] <- 3
+
 ###################################################################################
 #Virus prevalence (proportion infected) by Species Analyses 
 ###################################################################################
@@ -53,17 +63,13 @@ sp.virus.prev.model = glmmTMB(mean.plot.infection ~ FunctionalDiversity * Specie
                          + (1|Year:Block) + (1|Year:Block:Plot),
                           data=mean.spp.BYDV)
 summary(sp.virus.prev.model)
-car::Anova(sp.virus.prev.model, type=3)
+car::Anova(sp.virus.prev.model, type=3) #TABLE S2 (BYDV-PAV INFECTION PREVALENCE)
 
-emmeans(sp.virus.prev.model, pairwise~Species, type="response")
-emm <-emmeans(sp.virus.prev.model, pairwise~Species, type="response")
+# emmeans(sp.virus.prev.model, pairwise~Species, type="response")
+# emm <-emmeans(sp.virus.prev.model, pairwise~Species, type="response")
+# 
+# multcomp::cld(emm, Letters = LETTERS) #compact letter display for species
 
-multcomp::cld(emm, Letters = LETTERS) #compact letter display for species
-
-mean.spp.BYDV$weights <- 10
-
-#double check a.fatua [row 127] -> go back and make these out of the right number of samples:
-mean.spp.BYDV$weights[127] <- 7
 
 #############################################################################
 # Virus Prevalence (proportion infected) Analyses 
@@ -73,7 +79,7 @@ virus.prev.model = glmmTMB(mean.plot.infection ~GrassType * FunctionalDiversity 
                       weights=weights,
                       family= 'binomial', data=mean.spp.BYDV)
 summary(virus.prev.model)
-car::Anova(virus.prev.model,type=3)
+car::Anova(virus.prev.model,type=3) #TABLE S3 (BYDV-PAV INFECTION PREVALENCE)
 
 
 # #Post-hoct tests for effects of functional diversity on BYDV Prevalence (proportion infected)
@@ -81,16 +87,18 @@ emm_FD <-emmeans(virus.prev.model, ~FunctionalDiversity, type="response")
 emm_FD
 multcomp::cld(emm_FD, Letters = LETTERS) #compact letter display for functional diversity treatments
 contrast(emm_FD, "trt.vs.ctrl", ref = "G")
-#Post-hoc tests indicate no difference between functional diversity treatments
+#Both post-hoc tests indicate no difference between functional diversity treatments 
+#(this is consistent with infection probability analysis)
 
 #############################################################################
 # Virus Prevalence (proportion infected) X FG Richness Analyses 
 #############################################################################
 
 virus.prev.richness.model = glmmTMB(mean.plot.infection ~GrassType * fg_richness + Year
-                               + (1|Year:Block) + (1|Year:Block:Plot) + (1|Species),
-                                data=mean.spp.BYDV)
+                               + (1|Year:Block) + (1|Year:Block:Plot) + (1|Year:Block:Plot:Species) + (1|Species),
+                               weights=weights,
+                               family= 'binomial', data=mean.spp.BYDV)
 
 summary(virus.prev.richness.model)
-car::Anova(virus.prev.richness.model, type=3)
+car::Anova(virus.prev.richness.model, type=3) #TABLE S4 (BYDV-PAV INFECTION PREVALENCE)
 
