45dfb98c0edf61d22e3c630040a7e316cbc6bdbf
baguirre6 <baguirre9393@gmail.com>
2025-07-23 11:57:41 -0400

    ran supplemental analysis to test if production is correlated with infection (i.e. production drives infection)

--- 2_Analyses_and_Figures/biomass_workflow.R
+++ 2_Analyses_and_Figures/biomass_workflow.R
@@ -330,3 +330,11 @@ legume.figure
 
 # ggsave("4_Figures/FigureS3.pdf", legume.figure, 
 #        width = 6, height = 5)
+
+###################################################################################
+# Supplemental analyses to check if biomass is positively correlated with infection: 
+
+grass.model.sup <- lmer(mean.plot.infection ~ log(tot.plot.adj) * GrassType * FuncDiversity + Year + 
+                      (1|Year:Block), data= grass.regression.df)
+
+anova(grass.model.sup, type=3) 
