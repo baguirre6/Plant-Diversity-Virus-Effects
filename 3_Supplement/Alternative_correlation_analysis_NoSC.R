1df26d7bb6dab1e18b2067f1ab08f1b8472cd377
baguirre6 <baguirre9393@gmail.com>
2025-10-14 09:27:52 -0400

    ran supplemental analysis to test if the positive correlation between net community production and community-level bydv prevalence was driven by A. fatua

--- 3_Supplement/Alternative_correlation_analysis_NoSC.R
+++ 3_Supplement/Alternative_correlation_analysis_NoSC.R
@@ -71,3 +71,49 @@ anova(grass.model.no.af, type=3)
 
 #Post-hoc test for mean.plot infection x Grass Type interaction WITHOUT AVENA FATUA
 emtrends(grass.model.no.af, pairwise~GrassType, var = "mean.plot.infection", infer=TRUE)
+
+##############################################################
+# Is the positive correlation between net community productivity and community-level BYDV prevalence 
+#driven by A. fatua?
+
+source("2_Analyses_and_Figures/biomass_workflow.R")
+
+#Calculate total plot biomass: 
+biomass.data %>% 
+  filter(Species!= c('other', 'Avena fatua')) %>%  #exclude weeds
+  group_by(Year, Block, Plot, FuncDiversity, GrassType, planted_fg_richness) %>% 
+  summarize(
+    total_plot_biomass = sum(Biomass_grams.m2)) %>%
+  ungroup() -> plot_biomass.no.af
+
+#Calculate mean plot infection without A. fatua
+sp.grass.data %>%
+  filter(!Species == "Avena fatua") %>%  #exclude S. cereale
+  group_by(Year, Block, Plot, FuncDiversity, planted_fg_richness, GrassType)%>% 
+  summarise(mean.plot.infection = mean(mean.sp.infection, na.rm=TRUE)) -> plot.inf.no.af
+
+#join mean plot infection data to mean plot grass biomass data by the "Year" and "Plot" columns:
+plot_biomass.no.af %>% 
+  select(-c(FuncDiversity, planted_fg_richness, Block)) %>% 
+  left_join(plot.inf.no.af %>% select(Year, Plot, mean.plot.infection), by = c("Year", "Plot")) ->comm.regression.df.no.af
+
+
+#Check data frame structure: make planted fg richness factor
+comm.regression.df.no.af$planted_fg_richness <- as.factor(comm.regression.df.no.af$planted_fg_richness)
+
+################################
+
+# Look at the distribution of the community biomass data:
+hist(comm.regression.df.no.af$total_plot_biomass)
+# The data does not look normally distributed --> Log transform
+
+hist(log(comm.regression.df.no.af$total_plot_biomass))
+# Log transformed data is normally distributed
+
+#Community Productivity (WITHOUT AVENA FATUA)
+community.model.no.af <- lmer(log(total_plot_biomass) ~  mean.plot.infection * GrassType * FuncDiversity + Year + 
+                          (1|Year:Block), data= comm.regression.df.no.af)
+summary(community.model.no.af)
+anova(community.model.no.af, type=3)
+
+
